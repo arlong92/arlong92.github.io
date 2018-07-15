b1e3912578dabfa11525737e943f58660f4fa517
Ashley <arlong92@gmail.com>
2018-07-15 14:28:27 -0500

    added transit app

--- brave_transit/config/routes.rb
+++ brave_transit/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'locations#new'
+  get 'arlong92.github.io/brave_transit' => 'locations#new'
 
   resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
