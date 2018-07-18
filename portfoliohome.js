b4663811653d2f191cdd6e93b3fd15f9f20def30
Ashley <arlong92@gmail.com>
2018-07-18 15:02:19 -0500

    jquery3

--- portfoliohome.js
+++ portfoliohome.js
@@ -7,6 +7,6 @@ $('.col-md-6').hover(function() {
 	$(this).resizable();
 })
 
-$('#link').click(function() {
+$('.link').click(function() {
 	alert('Thank you for viewing my project. Click the back button to return to my site.')
 })
