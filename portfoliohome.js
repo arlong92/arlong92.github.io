3f3dcdc79c09a1209cc85713013e0a9f2d727f55
Ashley <arlong92@gmail.com>
2018-07-20 00:04:15 -0500

    fx6

--- portfoliohome.js
+++ portfoliohome.js
@@ -15,6 +15,6 @@ $('#pricelink').click(function() {
 	alert('Thank you for viewing my project. Click the back button to return to my site.')
 });
 
-$('.picture').click(function() {
+$('.picture').hover(function() {
 	$(this).animate('bounce', {times: 3}, 'slow');
 });
