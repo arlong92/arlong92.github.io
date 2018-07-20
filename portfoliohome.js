05345f889b7edcfd689e831f6ee5c115354fe4f9
Ashley <arlong92@gmail.com>
2018-07-20 00:10:30 -0500

    fixed

--- portfoliohome.js
+++ portfoliohome.js
@@ -4,15 +4,15 @@ $(function() {
 });
 
 $('#resizable').hover(function() {
-	$(this).resizable()
+	$(this).resizable();
 });
 
 $('#reslink').click(function() {
-	alert('Thank you for viewing my project. Click the back button to return to my site.')
+	alert('Thank you for viewing my project. Click the back button to return to my site.');
 });
 
 $('#pricelink').click(function() {
-	alert('Thank you for viewing my project. Click the back button to return to my site.')
+	alert('Thank you for viewing my project. Click the back button to return to my site.');
 });
 
 $('.picture').hover(function() {
