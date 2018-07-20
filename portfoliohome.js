5b06c7b578062fb3fe4894fe426477e863a5f314
Ashley <arlong92@gmail.com>
2018-07-20 00:35:41 -0500

    changed2

--- portfoliohome.js
+++ portfoliohome.js
@@ -15,10 +15,6 @@ $('#pricelink').click(function() {
 	alert('Thank you for viewing my project. Click the back button to return to my site.');
 });
 
-$('#picture').click(function() {
-	$(this).animate('bounce', {times: 2}, 'slow');
-});
-
-$('#picture2').click(function() {
+$('#picture').hover(function() {
 	$(this).animate('bounce', {times: 2}, 'slow');
 });
