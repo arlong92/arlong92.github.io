9b91eb8d528c8aefd253e8cc1305a4fec37b79a7
arlong92 <arlong92@gmail.com>
2018-11-06 22:00:41 -0600

    index

--- portfolioindex.js
+++ portfolioindex.js
@@ -1,2 +1,4 @@
-var username = prompt("Hi, what's your name?");
-alert("Welcome " + username + "! Thanks for visiting my portfolio!");
+window.setTimeout(function() {
+	var username = prompt("Hi, what's your name?");
+	alert("Welcome " + username + "! Thanks for visiting my portfolio!");
+}, 500);
